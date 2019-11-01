145a4e8664640477c73c5859d7fd3374bdaad95e
admin-SIMON <admin_simon@icloud.com>
2019-11-01 10:22:53 +0800

    Initial commit

---
+++ src/main/java/com/aliatry/sellabroad/SellAbroadApplication.java
@@ -0,0 +1,13 @@
+package com.aliatry.sellabroad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SellAbroadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SellAbroadApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/aliatry/sellabroad/SellAbroadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aliatry.sellabroad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SellAbroadApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
